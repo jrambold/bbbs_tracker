8670cac7cd113b7248f4e72dbd844fb4d9190924
jrambold <jrambold@gmail.com>
2018-04-04 23:25:49 -0600

    user match dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, :password, presence: true
   validates :username, presence: true, uniqueness: true
   has_secure_password
-  has_many :matches
+  has_many :matches, dependent: :destroy
   has_many :user_contacts
   has_many  :contacts, through: :user_contacts
   enum group: ['default', 'teamlead', 'super', 'admin']
